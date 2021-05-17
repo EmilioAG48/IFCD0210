0d6b30f70d215f3a52a3f6afa6068c64eadac548
EmilioAG48 <you@example.com>
2021-05-17 17:11:11 +0200

    s

---
+++ Examen 20_5_2021/scripts.js
@@ -0,0 +1,12 @@
+console.log("examen");
+
+const divJS = document.getElementById("javascript");
+
+// Ejercicio 2
+const funcion1 = (cadena1, cadena2) => {
+  let mensaje = "";
+  mensaje = cadena1 + " " + cadena2;
+  console.log(mensaje.toLowerCase());
+};
+
+funcion1("Hola", "Mundo");
